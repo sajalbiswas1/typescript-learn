7871f8fae7db8d5f950aee7314cbfb56e9cc9af0
sajalbiswas1 <sajalb1899@gmail.com>
2024-03-25 21:06:05 +0600

    arry-type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,9 @@
+// let userNames: string[];
+var userNames;
+userNames = ['sajal', 'rahim', 'korim'];
+console.log(userNames[1]);
+userNames.sort();
+console.log(userNames);
+var multipletype;
+multipletype = [12, "sajal", "biswas"];
+console.log(multipletype);
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,13 @@
+// let userNames: string[];
+let userNames: Array<string>
+
+userNames = ['sajal', 'rahim', 'korim'];
+console.log(userNames[1])
+
+userNames.sort()
+console.log(userNames)
+
+let multipletype: (string | number)[];
+
+multipletype = [12, "sajal", "biswas"]
+console.log(multipletype)
