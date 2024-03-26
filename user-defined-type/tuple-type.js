8a403c5027ffd4493a063c9df53828b9684e9f34
sajalbiswas1 <sajalb1899@gmail.com>
2024-03-26 06:55:48 +0600

    tuple type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,6 @@
+// mix data
+var user;
+user = [101, "Sajal"];
+console.log(user);
+user.push(102, "Mukta");
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mix data
+
+let user: [number, string];
+user = [101, "Sajal"];
+
+console.log(user)
+user.push(102,"Mukta")
+console.log(user)
